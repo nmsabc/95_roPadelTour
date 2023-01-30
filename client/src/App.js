2e45037070c4f2e59a5c545489c9293b4f20500c
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-01-31 00:08:40 +0100

    we show now on webPage all records in the DB

--- client/src/App.js
+++ client/src/App.js
@@ -10,18 +10,52 @@ function App() {
   useEffect(() => {
     axios.get("http://localhost:3213/rankings").then((response) => {
       // console.log(response.data);
-      console.log("Number of rankings in the DB is : ", response.data.length);
+      console.log("Number of RANKINGS in the DB is : ", response.data.length);
       setListOfRankings(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get("http://localhost:3213/players").then((response) => {
+      // console.log(response.data);
+      console.log("Number of PLAYERS in the DB is : ", response.data.length);
+      setListOfPlayers(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get("http://localhost:3213/posts").then((response) => {
+      // console.log(response.data);
+      console.log("Number of POSTS in the DB is : ", response.data.length);
+      setListOfPosts(response.data);
     });
   }, []);
 
   return (
     <div className="App">
-      <p>FE works !</p>
+      <p>List of Rankings!</p>
       {listOfRankings.map((value, key) => {
         return (
           <div className="ranking">
-            <div className="name">Ranking_ {value.r_name} _with_ {value.r_description} _is in category_ {value.r_category}</div>
+            <div className="rankinginf">Ranking_ {value.r_name} _with_ {value.r_description} _is in category_ {value.r_category}</div>
+          </div>
+        );
+      })}
+
+    <p>List of Players!</p>
+      {listOfPlayers.map((value, key) => {
+        return (
+          <div className="player">
+            <div className="playerinfo">Player_ {value.first_name} _living in _ {value.address_city} _has_ {value.points} _poins.</div>
+          </div>
+        );
+      })}
+
+    <p>List of Posts!</p>
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="postinfo">The user_ {value.username} _has written_ {value.title} _containing_ {value.postText}</div>
           </div>
         );
       })}
