7a06abd3a4b02eb338bfe81a81ebf006122d47df
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-01-30 22:38:39 +0100

    added the first call and this is blocked by CORS policy

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import axios from "axios";
+import { useEffect } from 'react';
 
 function App() {
+      useEffect(() => {
+        axios.get("http://localhost:3213/rankings").then((response) =>{
+          console.log(response)
+        });
+      }, []);
+
   return (
     <div className="App">
     </div>
