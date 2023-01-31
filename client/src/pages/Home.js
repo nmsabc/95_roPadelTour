f4e582f4ac754c6c79ad14011adbca080832da20
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-01-31 15:44:30 +0100

    two routes are working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [listOfPlayers, setListOfPlayers] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3213/players").then((response) => {
+      // console.log(response.data);
+      console.log("Number of PLAYERS in the DB is : ", response.data.length);
+      setListOfPlayers(response.data);
+    });
+  }, []);
+
   return (
     <div>
-      
+      <p>welcome home </p>
+      <p>List of Players!</p>
+      {listOfPlayers.map((value, key) => {
+        return (
+          <div className="player_h" key={key}>
+            <div className="playerinfo_H">Player_ {value.first_name} _living in _ {value.address_city} _has_ {value.points} _poins.</div>
+          </div>
+        );
+      })}
     </div>
   )
 }
 
-export default Home
+export default Home;
