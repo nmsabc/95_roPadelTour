27acad374c09221e3001566b4ba35d77b30a8ea0
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-01-31 20:12:41 +0100

    added formik and started to work on the forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import "./App.css";
-import {Routes, Route, BrowserRouter as Router } from "react-router-dom"
+import { Routes, Route, BrowserRouter as Router, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
-import OldAppStart from "./pages/Old_App_Start"
+import OldAppStart from "./pages/Old_App_Start";
 import HelloToTheWorld from "./pages/HelloToTheWorld";
+import CreatePost from "./pages/CreatePost";
+import CreatePlayer from "./pages/CreatePlayer";
+import CreateRanking from "./pages/CreateRanking";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <div className="top-menu">
+          <Link to="/">Home </Link>
+          <Link to="/createPost">New Post </Link>
+          <Link to="/createPlayer">New Player </Link>
+          <Link to="/createRanking">New Ranking</Link>
+        </div>
+
         <Routes>
           <Route path="/" element={<Home />} exact />
           <Route path="/OldHome" element={<OldAppStart />} exact />
-          <Route path="/hello-world" element={<HelloToTheWorld/>} exact />
+          <Route path="/hello-world" element={<HelloToTheWorld />} exact />
+          <Route path="/createPost" element={<CreatePost />} exact />
+          <Route path="/createPlayer" element={<CreatePlayer />} exact />
+          <Route path="/createRanking" element={<CreateRanking />} exact />
         </Routes>
       </Router>
     </div>
