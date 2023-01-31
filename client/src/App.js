90391e4c812f30e5f9be5f5b1660145bd6da06b0
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-01-31 17:58:23 +0100

    added hello-world with props processing

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import {Routes, Route, BrowserRouter as Router } from "react-router-dom"
 
 import Home from "./pages/Home";
 import OldAppStart from "./pages/Old_App_Start"
+import HelloToTheWorld from "./pages/HelloToTheWorld";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/OldHome" element={<OldAppStart />} />
+          <Route path="/" element={<Home />} exact />
+          <Route path="/OldHome" element={<OldAppStart />} exact />
+          <Route path="/hello-world" element={<HelloToTheWorld/>} exact />
         </Routes>
       </Router>
     </div>
---
+++ client/src/pages/HelloToTheWorld.js
@@ -0,0 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as ReactDOMClient from 'react-dom/client';
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function PostDate(props) {
+    return <div className="Comment-date">{formatDate(props.date)}</div>;
+  }
+
+  function PostCurrentDate(props) {
+    console.log('1...',props.cdate)
+    return <div className="Current-date">{formatDate(props.cdate)}</div>;
+  }
+    
+function PostText(props) {
+  return <div className="comment-full-text">{props.text}</div>;
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      this was done on: <PostDate date={props.date} /> 
+      <UserInfo user={props.author} />
+      <PostText text={props.text} />
+      And the just now date is: 
+      <PostCurrentDate cdate={props.cdate} /> 
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date('2023-01-31'),
+  cdate: new Date(),
+  text: "I hope you enjoy learning React! __ THIS IS NOT PERFECT but WORKS ___",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "http://placekitten.com/g/64/64",
+  },
+};
+
+function HelloToTheWorld() {
+  // const root = ReactDOM.createRoot(document.getElementById("root"));
+  const root = ReactDOMClient.createRoot(document.getElementById("root"));
+
+  root.render(
+    <Comment date={comment.date} text={comment.text} author={comment.author} cdate={comment.cdate} />
+  );
+  return <div className="the-info" id="root"></div>;
+}
+
+export default HelloToTheWorld;
