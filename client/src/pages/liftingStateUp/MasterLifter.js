a1588bfcc863536b311eab30f69cd10388086fc7
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-02-04 16:18:26 +0100

    staring starte betw compo stage 2 done

--- client/src/pages/liftingStateUp/MasterLifter.js
+++ client/src/pages/liftingStateUp/MasterLifter.js
@@ -1,18 +1,18 @@
-
 // https://beta.reactjs.org/learn/sharing-state-between-components
 
 import React from "react";
 import { useState } from "react";
 
-function Panel({ title, children }) {
-  const [isActive, setIsActive] = useState(false);
+function Panel({ title, children, isActive }) {
+  //   const [isActive, setIsActive] = useState(false);
+  const [isInActive, reActivate] = useState(false);
   return (
     <section className="master-lifter-panel">
-      <h3>{title}</h3>
-      {isActive ? (
+      <div className="master-lifter-header">{title}</div>
+      {isActive ?  (
         <p>{children}</p>
       ) : (
-        <button onClick={() => setIsActive(true)}>Show</button>
+        <button onClick={() => reActivate(true)}>Show</button>
       )}
     </section>
   );
@@ -22,16 +22,26 @@ function MasterLifter() {
   return (
     <>
       <h2>Almaty, Kazakhstan</h2>
-      <Panel title="About" className="master-lifter-panel">
+      <Panel title="About" className="master-lifter-panel" isActive={true}>
         With a population of about 2 million, Almaty is Kazakhstan's largest
         city. From 1929 to 1997, it was its capital city.
       </Panel>
-      <Panel title="Etymology" className="master-lifter-panel">
+
+      <Panel title="Etymology" className="master-lifter-panel" isActive={true}>
         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
         "apple" and is often translated as "full of apples". In fact, the region
         surrounding Almaty is thought to be the ancestral home of the apple, and
         the wild <i lang="la">Malus sieversii</i> is considered a likely
         candidate for the ancestor of the modern domestic apple.
+      </Panel>
+
+      <Panel
+        title="German translation"
+        className="master-lifter-panel"
+        isActive={true}
+      >
+        Mit rund 2 Millionen Einwohnern ist Almaty die größte Stadt Kasachstans.
+        Stadt. Von 1929 bis 1997 war sie die Hauptstadt des Landes.
       </Panel>
     </>
   );
