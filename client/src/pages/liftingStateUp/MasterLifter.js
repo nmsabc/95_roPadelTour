82e10abf37f986693ff316cb4e27e76298bc3898
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-02-04 15:54:52 +0100

    state sharing betw compo > stage 1 complete

---
+++ client/src/pages/liftingStateUp/MasterLifter.js
@@ -0,0 +1,40 @@
+
+// https://beta.reactjs.org/learn/sharing-state-between-components
+
+import React from "react";
+import { useState } from "react";
+
+function Panel({ title, children }) {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <section className="master-lifter-panel">
+      <h3>{title}</h3>
+      {isActive ? (
+        <p>{children}</p>
+      ) : (
+        <button onClick={() => setIsActive(true)}>Show</button>
+      )}
+    </section>
+  );
+}
+
+function MasterLifter() {
+  return (
+    <>
+      <h2>Almaty, Kazakhstan</h2>
+      <Panel title="About" className="master-lifter-panel">
+        With a population of about 2 million, Almaty is Kazakhstan's largest
+        city. From 1929 to 1997, it was its capital city.
+      </Panel>
+      <Panel title="Etymology" className="master-lifter-panel">
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
+        "apple" and is often translated as "full of apples". In fact, the region
+        surrounding Almaty is thought to be the ancestral home of the apple, and
+        the wild <i lang="la">Malus sieversii</i> is considered a likely
+        candidate for the ancestor of the modern domestic apple.
+      </Panel>
+    </>
+  );
+}
+
+export default MasterLifter;
