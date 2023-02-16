06314eab9b24e091fbc07b88d3fb27e6077d24f8
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-02-16 22:58:02 +0100

    nicely passed the props back and fwd between compo

--- client/src/pages/InformationCard.js
+++ client/src/pages/InformationCard.js
@@ -18,17 +18,13 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 var ld = require("lodash");
 
-function InformationCard({ note }) {
+function InformationCard({ note, changeFavorite, favIcon}) {
   const [expanded, setExpanded] = React.useState(false);
-  const [favIcon, setFavIcon] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
-  const changeFavoriteIcon = () => {
-    setFavIcon(!favIcon);
-  };
 
   return (
     <div>
@@ -45,26 +41,21 @@ function InformationCard({ note }) {
             </IconButton>
           }
           title={note.title}
-          subheader={note.description}
+          subheader={note.subtitle}
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
             {note.description}
-            <br /> This impressive paella is a perfect party dish and a fun meal
-            to cook together with your guests. Add 1 cup of frozen peas along
-            with the mussels, if you like.
           </Typography>
         </CardContent>
 
         <CardActions disableSpacing>
           <IconButton
             onClick={() => {
-              console.log("fav this... ", note.title, favIcon);
-              changeFavoriteIcon();
+              changeFavorite(note.id);
             }}
           >
-            {favIcon ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-            {/* {favIcon ? FavoriteIcon : FavoriteBorderIcon} */}
+            {note.favorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
           </IconButton>
           <IconButton onClick={() => console.log("isShared", note.title)}>
             <ShareIcon />
--- client/src/pages/Palmares.js
+++ client/src/pages/Palmares.js
@@ -6,43 +6,65 @@ import RecipeReviewCard from "../Not_used/RecipeReviewCard";
 
 const listPalmares = [
   {
-    id: 1,
+    id: 0,
     title: "Axtra info 1",
-    description: "some form of dEsC_1",
+    subtitle: "this is how we do it ... ",
+    description:
+      "some form of dEsC_1, This impressive paella is a perfect party dish is a perfect party dish is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
     note: "note 1 ...",
+    favorite: false,
+  },
+  {
+    id: 1,
+    title: "Bxtra info 2",
+    subtitle: "this is how we do it ... ",
+    description:
+      "some form of dEsC_2, This impressive paella is a perfect party dish and a fun meal to cook together with your fun meal to cook together with your fun meal to cook together with your fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+    note: "note 2 ...",
+    favorite: false,
   },
   {
     id: 2,
-    title: "Bxtra info 2",
-    description: "some form of dEsC_2",
-    note: "note 2 ...",
+    title: "Cxtra info 3",
+    subtitle: "this is how we do it ... ",
+    description:
+      "some form of dEsC_3, This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup 1 cup 1 cup 1 cup 1 cup 1 cup 1 cup of frozen peas along with the mussels, if you like.",
+    note: "note 3 ...",
+    favorite: false,
   },
   {
     id: 3,
-    title: "Cxtra info 3",
-    description: "some form of dEsC_3",
-    note: "note 3 ...",
+    title: "Dxtra info 4",
+    subtitle: "this is how we do it ... ",
+    description:
+      "some form of dEsC_4, This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas frozen peas frozen peas frozen peas frozen peas along with the mussels, if you like.",
+    note: "note 4 ...",
+    favorite: false,
   },
   {
     id: 4,
-    title: "Dextra info 4",
-    description: "some form of dEsC_4",
-    note: "note 4 ...",
-  },
-  {
-    id: 5,
     title: "Extra info 5",
-    description: "some form of dEsC_5",
+    subtitle: "this is how we do it ... ",
+    description:
+      "some form of dEsC_5, This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, along with the mussels, along with the mussels, along with the mussels, along with the mussels, along with the mussels, if you like.",
     note: "note 5 ...",
+    favorite: false,
   },
 ];
 function Palmares() {
+  const [favIcon, setFavIcon] = React.useState(false);
+
+  const changeFavorite = (id) => {
+    setFavIcon(!favIcon);
+    listPalmares[id].favorite = !listPalmares[id].favorite;
+  };
+
   return (
     <Container>
       <Grid container spacing={3}>
         {listPalmares.map((note) => (
           <Grid item key={note.id} xs={12} md={6} lg={4}>
-            <InformationCard note={note} />
+            <InformationCard note={note} changeFavorite={changeFavorite} />
           </Grid>
         ))}
       </Grid>
