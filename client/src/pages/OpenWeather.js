59cfbcb3ba61ee789e89ac18cbdfd4fca8928037
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-02-19 12:27:47 +0100

    weather 3.0 is NOT OK

--- client/src/pages/OpenWeather.js
+++ client/src/pages/OpenWeather.js
@@ -9,16 +9,29 @@ function OpenWeather({ weatherCity, loopCities }) {
   const [temp, setTemp] = useState(0);
   const [weatherDescription, setWeatherDescription] = useState("");
   const [windSpeed, setWindSpeed] = useState("");
+  let owm_ver = "2.5";
 
   useEffect(() => {
     axios
-      .get(`http://localhost:3213/weather/${weatherCity}`)
+      .get(`http://localhost:3213/weather/${weatherCity}`, {
+        headers: {
+          owm_ver: owm_ver,
+        },
+      })
       .then((res_w) => {
-        setTemp(round(res_w.data.main.temp - 273.15, 3));
-        setWeatherDescription(res_w.data.weather[0].description);
-        setWindSpeed(res_w.data.wind.speed)
+        if (owm_ver === "2.5") {
+          setTemp(round(res_w.data.main.temp - 273.15, 3));
+          setWeatherDescription(res_w.data.weather[0].description);
+          setWindSpeed(res_w.data.wind.speed);
+        } 
+        if (owm_ver === "3.0") {
+          console.log(res_w)
+          setTemp(res_w.data.current.temp);
+          setWeatherDescription(res_w.data.current.temp.description);
+          setWindSpeed(res_w.data.current.wind_speed);
+        }
       });
-  }, [weatherCity]);
+  }, [weatherCity, owm_ver]);
 
   return (
     <>
@@ -27,7 +40,7 @@ function OpenWeather({ weatherCity, loopCities }) {
       {" in "}
       {ld.capitalize(weatherCity)}
       {" with "}
-      {temp} 
+      {temp}
       {" °C and wind speed of "}
       {windSpeed}
       <LoopIcon onClick={() => loopCities(weatherCity)} />
--- server/routes/OpenWeatherMap.js
+++ server/routes/OpenWeatherMap.js
@@ -3,13 +3,24 @@ const router = express.Router();
 const { validateToken } = require("../middlewares/AuthMiddleware");
 require("dotenv").config();
 
+const url30 = "https://api.openweathermap.org/data/3.0/onecall";
+const met = "metric";
+
+//tm si baden lat si lon
+const lat_tm = 45.806107;
+const lon_tm = 21.237475;
+const lat_b = 48.00;
+const lon_b = 16.21;
+
 const timisoara = `https://api.openweathermap.org/data/2.5/weather?q=Timisoara,ro&APPID=${process.env.RPT_API_KEY}`;
 const london = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${process.env.RPT_API_KEY}`;
 const baden = `https://api.openweathermap.org/data/2.5/weather?lat=48.00&lon=16.21&APPID=${process.env.RPT_API_KEY}`;
-const baden30 =`https://api.openweathermap.org/data/3.0/onecall?lat=48.00&lon=16.21&appid=${process.env.RPT_API_KEY}`
+const baden30 = `${url30}?lat=${lat_b}&lon=${lon_b}&units=${met}&appid=${process.env.RPT_API_KEY}`;
+const timisoara30 = `${url30}?lat=${lat_tm}&lon=${lon_tm}&units=${met}&appid=${process.env.RPT_API_KEY}`;
 
 router.get("/:city", async (req, res) => {
   city_req = req.params.city;
+  owm_ver = req.header("owm_ver");
   var requestOptions = {
     method: "POST",
     headers: {
@@ -17,28 +28,35 @@ router.get("/:city", async (req, res) => {
     },
     redirect: "follow",
   };
+  let result = await getApi(requestOptions, city_req, owm_ver);
   try {
-    let result = await getApi(requestOptions, city_req);
     res.status(200).json(result);
   } catch (err) {
     return res.status(500).json(err);
   }
 });
 
-function getApi(requestOptions, ci) {
-  if (ci === "baden") {
+async function getApi(requestOptions, ci, owm_ver) {
+  if (ci === "baden" && owm_ver === "2.5") {
     city_to_req = baden;
   }
-  if (ci === "timisoara") {
+  if (ci === "baden" && owm_ver === "3.0") {
+    city_to_req = baden30;
+  }
+  if (ci === "timisoara" && owm_ver === "2.5") {
     city_to_req = timisoara;
   }
-  if (ci === "london") {
+  if (ci === "timisoara" && owm_ver === "3.0") {
+    city_to_req = timisoara30;
+  }
+  if (ci === "london" && owm_ver === "2.5") {
     city_to_req = london;
   }
+  console.log(city_to_req)
 
-  return fetch(city_to_req, requestOptions).then((res) => {
-    return res.json();
-  });
+
+  const res = await fetch(city_to_req, requestOptions);
+  return await res.json();
 }
 
 module.exports = router;
