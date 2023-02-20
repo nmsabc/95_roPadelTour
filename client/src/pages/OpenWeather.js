b636ea8dd19500950edb85f04ede1c3e702cca50
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-02-20 23:20:21 +0100

    mongoDB without a mongo client and wih a rpt coll

--- client/src/pages/OpenWeather.js
+++ client/src/pages/OpenWeather.js
@@ -4,12 +4,39 @@ import React, { useEffect, useState } from "react";
 import { round } from "lodash";
 import LoopIcon from "@mui/icons-material/Loop";
 let ld = require("lodash");
+// const MongoClient = require("mongodb").MongoClient;
+
+function insertDataInMongo(weatherCity, temp, windSpeed) {
+  console.log("almost there ...")
+  // Connect to MongoDB
+  // const uri = "mongodb://root:secret@localhost:27017/mongo-rpt";
+  // const client = new MongoClient(uri, { useNewUrlParser: true });
+  // client.connect((err) => {
+  //   const collection = client.db("mongo-rpt").collection("weather");
+
+  //   // Insert a document with timestamp, temp, and location
+  //   collection.insertOne(
+  //     {
+  //       timestamp: new Date(),
+  //       location: weatherCity,
+  //       temp: temp,
+  //       windSpeed: windSpeed,
+  //     },
+  //     (err, result) => {
+  //       if (err) throw err;
+  //       console.log("Document inserted");
+  //       client.close();
+  //     }
+  //   );
+  // });
+}
 
 function OpenWeather({ weatherCity, loopCities }) {
   const [temp, setTemp] = useState(0);
   const [weatherDescription, setWeatherDescription] = useState("");
   const [windSpeed, setWindSpeed] = useState("");
   let owm_ver = "2.5";
+  let mongo = true;
 
   useEffect(() => {
     axios
@@ -23,12 +50,15 @@ function OpenWeather({ weatherCity, loopCities }) {
           setTemp(round(res_w.data.main.temp - 273.15, 3));
           setWeatherDescription(res_w.data.weather[0].description);
           setWindSpeed(res_w.data.wind.speed);
-        } 
+        }
         if (owm_ver === "3.0") {
-          console.log(res_w)
+          console.log(res_w);
           setTemp(res_w.data.current.temp);
           setWeatherDescription(res_w.data.current.temp.description);
           setWindSpeed(res_w.data.current.wind_speed);
+        }
+        if (mongo) {
+          insertDataInMongo(weatherCity, temp, windSpeed);
         }
       });
   }, [weatherCity, owm_ver]);
