a9b89129ab536c652ea4a65bc6de0c2d1826d443
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-02-21 02:27:21 +0100

    arranged savePadel Game Info page

--- client/src/pages/Padel/SavePadelGameInfo.js
+++ client/src/pages/Padel/SavePadelGameInfo.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { Container } from "@mui/material";
+import Box from "@mui/material/Box";
 
 const SavePadelGameInfo = () => {
   const [team1Name, setTeam1Name] = useState("");
@@ -35,67 +36,114 @@ const SavePadelGameInfo = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Container className="t1">
-      <TextField
-        label="Team 1 Name"
-        value={team1Name}
-        onChange={(event) => setTeam1Name(event.target.value)}
-      />
+    <Box
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <form onSubmit={handleSubmit}>
+        <div>
+          <TextField
+            label="Team 1"
+            id="outlined-number"
+            required
+            value={team1Name}
+            onChange={(event) => setTeam1Name(event.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
 
-      <TextField
-        label="Set 1 Team 1 Score"
-        type="number"
-        value={set1Team1Score}
-        onChange={(event) => setSet1Team1Score(event.target.value)}
-      />
-
-      <TextField
-        label="Set 2 Team 1 Score"
-        type="number"
-        value={set2Team1Score}
-        onChange={(event) => setSet2Team1Score(event.target.value)}
-      />
-
-      <TextField
-        label="Set 3 Team 1 Score"
-        type="number"
-        value={set3Team1Score}
-        onChange={(event) => setSet3Team1Score(event.target.value)}
-      />
-      </Container>
-      <br />
-      <Container className="t2">
-        <TextField
-          label="Team 2 Name"
-          value={team2Name}
-          onChange={(event) => setTeam2Name(event.target.value)}
-        />
-        <TextField
-          label="Set 1 Team 2 Score"
-          type="number"
-          value={set1Team2Score}
-          onChange={(event) => setSet1Team2Score(event.target.value)}
-        />
-
-        <TextField
-          label="Set 2 Team 2 Score"
-          type="number"
-          value={set2Team2Score}
-          onChange={(event) => setSet2Team2Score(event.target.value)}
-        />
-
-        <TextField
-          label="Set 3 Team 2 Score"
-          type="number"
-          value={set3Team2Score}
-          onChange={(event) => setSet3Team2Score(event.target.value)}
-        />
-      </Container>
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-    </form>
+          <TextField
+            label="Team 2"
+            id="outlined-number"
+            required
+            value={team2Name}
+            onChange={(event) => setTeam2Name(event.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+        <div>
+          <div>
+            <TextField
+              label="Set 1 Team 1 Score"
+              type="number"
+              value={set1Team1Score}
+              onChange={(event) => setSet1Team1Score(event.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+            />
+            <TextField
+              label="Set 1 Team 2 Score"
+              type="number"
+              value={set1Team2Score}
+              onChange={(event) => setSet1Team2Score(event.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+            />
+            <div>
+              <TextField
+                id="outlined-number"
+                label="Set 2 Team 1 Score"
+                type="number"
+                value={set2Team1Score}
+                onChange={(event) => setSet2Team1Score(event.target.value)}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                required
+              />
+              <TextField
+                id="outlined-number"
+                label="Set 2 Team 2 Score"
+                type="number"
+                value={set2Team2Score}
+                onChange={(event) => setSet2Team2Score(event.target.value)}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                required
+              />
+            </div>
+            <div>
+              <TextField
+                id="outlined-number"
+                label="Set 3 Team 1 Score"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={set3Team1Score}
+                onChange={(event) => setSet3Team1Score(event.target.value)}
+              />
+              <TextField
+                id="outlined-number"
+                label="Set 3 Team 2 Score"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={set3Team2Score}
+                onChange={(event) => setSet3Team2Score(event.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div align="center">
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </div>
+      </form>
+    </Box>
   );
 };
 
