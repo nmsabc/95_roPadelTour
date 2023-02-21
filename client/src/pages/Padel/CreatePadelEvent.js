735df74b81ef912bfe8b47a7677c309adeaeca03
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-02-21 01:43:13 +0100

    padel event done and formit darkula changed

--- client/src/pages/Padel/CreatePadelEvent.js
+++ client/src/pages/Padel/CreatePadelEvent.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
 
-
-
+import InputLabel from "@mui/material/InputLabel";
+import Box from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 function CreatePadelEvent() {
   const [eventName, setEventName] = useState("");
@@ -21,14 +24,18 @@ function CreatePadelEvent() {
     });
   };
 
+  const handleChange = (event) => {
+    setEventType(event.target.value);
+  };
+
   return (
     <Container maxWidth="sm">
       <form onSubmit={handleSubmit}>
         <TextField
-          label="Event Name"
-          fullWidth
-          margin="normal"
+          label="Please provide a name for this Event"
+          required
           value={eventName}
+          fullWidth
           onChange={(event) => setEventName(event.target.value)}
         />
         <TextField
@@ -39,24 +46,29 @@ function CreatePadelEvent() {
           value={startDate}
           onChange={(event) => setStartDate(event.target.value)}
         />
-        <TextField
-          select
-          label="Event Type"
-          fullWidth
-          margin="normal"
-          value={eventType}
-          onChange={(event) => setEventType(event.target.value)}
-        >
-          <option value="Training">Training</option>
-          <option value="Simple Tournament">Simple Tournament</option>
-          <option value="Championship">Championship</option>
-        </TextField>
+
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Event Type:</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={eventType}
+              label="eventType"
+              onChange={handleChange}
+            >
+              <MenuItem value="Training">Training</MenuItem>
+              <MenuItem value="Simple Tournament">Simple Tournament</MenuItem>
+              <MenuItem value="Championship">Championship</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
         <Button type="submit" variant="contained" color="primary">
           Save
         </Button>
       </form>
     </Container>
   );
-};
+}
 
 export default CreatePadelEvent;
