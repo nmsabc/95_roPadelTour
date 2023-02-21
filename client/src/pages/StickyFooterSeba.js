d6399358d02143b88f06049e3f214ab597215b73
Sebastian Ivan <sebastian.ivan@gmail.com>
2023-02-21 09:28:08 +0100

    loop aver the full list of cities in OpemWeatherMap

--- client/src/pages/StickyFooterSeba.js
+++ client/src/pages/StickyFooterSeba.js
@@ -3,20 +3,38 @@ import Typography from "@mui/material/Typography";
 import OpenWeather from "./OpenWeather";
 
 export default function StickyFooterSeba() {
-  const citiesList = ["baden", "timisoara" , "london"];
+  const citiesList = ["baden", "timisoara", "london"];
   const [weatherCity, setWeatherCity] = useState("baden");
+  const [cityIndex, setCityIndex] = useState(0);
 
-  const loopCities = (city) => {
+  function circularLoop(list) {
+    let currentIndex = 0;
+    return function () {
+      let currentItem = list[currentIndex];
+      currentIndex = (currentIndex + 1) % list.length;
+      return currentItem;
+    };
+  }
+
+  const loopFirstTwoCities = (city) => {
     if (city === citiesList[0] || city === "") {
       setWeatherCity(citiesList[1]);
     } else {
       setWeatherCity(citiesList[0]);
     }
   };
+
+  const loopCities = (city) => {
+    if (city === citiesList[cityIndex] || city === "") {
+      let nextIndex = (cityIndex + 1) % citiesList.length;
+      setCityIndex(nextIndex);
+      setWeatherCity(citiesList[nextIndex]);
+    }
+  };
   return (
     <div>
       <Typography>
-        <OpenWeather weatherCity={weatherCity} loopCities={loopCities} /> 
+        <OpenWeather weatherCity={weatherCity} loopCities={loopCities} />
       </Typography>
       <Typography>
         Go padel Hard!
